fb519bf2c7ab5996183a3e052c6104c47c41e1bf
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2024-05-25 07:31:58 +0300

    handlers_test.go: test stale MC data handling

--- handlers_test.go
+++ handlers_test.go
@@ -63,6 +63,10 @@ func TestExternalCoordinatorServer(t *testing.T) {
 	tempDir := t.TempDir()
 
 	config := &Config{
+		Server: ServerConfig{
+			HistoryThresholdDuration: 10 * time.Minute,
+			StaleDataCleanupInterval: time.Second,
+		},
 		Database: DatabaseConfig{
 			DatabaseDirPath: tempDir,
 			DatabaseFile:    "test.db",
@@ -78,22 +82,24 @@ func TestExternalCoordinatorServer(t *testing.T) {
 	}
 	defer cleanupDB(db)
 
-	server := NewExternalCoordinatorServer(db)
+	server := NewExternalCoordinatorServer(config, db)
 
 	t.Run("RegisterMissionControl", func(t *testing.T) {
 		// Case 1: Valid request with nodeFrom, nodeTo and history data.
 		t.Run("ValidRequest", func(t *testing.T) {
 			nodeFrom, nodeTo := generateTestKeys(t)
+			failTime := time.Now().Unix()
+			successTime := time.Now().Unix()
 			req := &ecrpc.RegisterMissionControlRequest{
 				Pairs: []*ecrpc.PairHistory{
 					{
 						NodeFrom: nodeFrom,
 						NodeTo:   nodeTo,
 						History: &ecrpc.PairData{
-							FailTime:       1,
+							FailTime:       failTime,
 							FailAmtSat:     100,
 							FailAmtMsat:    1000,
-							SuccessTime:    2,
+							SuccessTime:    successTime,
 							SuccessAmtSat:  200,
 							SuccessAmtMsat: 2000,
 						},
@@ -201,8 +207,10 @@ func TestExternalCoordinatorServer(t *testing.T) {
 		t.Run("ValidRequestWithData", func(t *testing.T) {
 			err = clearDatabase(db)
 			require.NoError(t, err)
-			server := NewExternalCoordinatorServer(db)
+			server := NewExternalCoordinatorServer(config, db)
 			nodeFrom, nodeTo := generateTestKeys(t)
+			failTime := time.Now().Unix()
+			successTime := time.Now().Unix()
 			_, err = server.RegisterMissionControl(
 				context.Background(),
 				&ecrpc.RegisterMissionControlRequest{
@@ -211,10 +219,10 @@ func TestExternalCoordinatorServer(t *testing.T) {
 							NodeFrom: nodeFrom,
 							NodeTo:   nodeTo,
 							History: &ecrpc.PairData{
-								FailTime:       1,
+								FailTime:       failTime,
 								FailAmtSat:     100,
 								FailAmtMsat:    1000,
-								SuccessTime:    2,
+								SuccessTime:    successTime,
 								SuccessAmtSat:  200,
 								SuccessAmtMsat: 2000,
 							},
@@ -238,11 +246,11 @@ func TestExternalCoordinatorServer(t *testing.T) {
 			require.Equal(t, nodeTo, resp.Pairs[0].NodeTo)
 		})
 
-		// Case 1: Valid request with no data in the database.
+		// Case 2: Valid request with no data in the database.
 		t.Run("ValidRequestWithoutData", func(t *testing.T) {
 			err = clearDatabase(db)
 			require.NoError(t, err)
-			server := NewExternalCoordinatorServer(db)
+			server := NewExternalCoordinatorServer(config, db)
 			resp, err := server.QueryAggregatedMissionControl(
 				context.Background(),
 				&ecrpc.QueryAggregatedMissionControlRequest{},
@@ -250,6 +258,227 @@ func TestExternalCoordinatorServer(t *testing.T) {
 			require.NoError(t, err)
 			require.NotNil(t, resp)
 			require.Len(t, resp.Pairs, 0)
+		})
+	})
+
+	t.Run("RunCleanupRoutine", func(t *testing.T) {
+		// Clear database before running the test.
+		err := clearDatabase(db)
+		require.NoError(t, err)
+
+		nodeFrom, nodeTo := generateTestKeys(t)
+
+		failTime1 := time.Now().Unix()
+		successTime1 := time.Now().Unix()
+		failTime2 := time.Now().Add(-15 * time.Minute).Unix()
+		successTime2 := time.Now().Add(-12 * time.Minute).Unix()
+
+		// Insert test data into the database.
+		_, err = server.RegisterMissionControl(context.Background(), &ecrpc.RegisterMissionControlRequest{
+			Pairs: []*ecrpc.PairHistory{
+				{
+					NodeFrom: nodeFrom,
+					NodeTo:   nodeTo,
+					History: &ecrpc.PairData{
+						FailTime:       failTime1,
+						FailAmtSat:     100,
+						FailAmtMsat:    1000,
+						SuccessTime:    successTime1,
+						SuccessAmtSat:  200,
+						SuccessAmtMsat: 2000,
+					},
+				},
+				{
+					NodeFrom: nodeFrom,
+					NodeTo:   nodeTo,
+					History: &ecrpc.PairData{
+						FailTime:       failTime2,
+						FailAmtSat:     100,
+						FailAmtMsat:    1000,
+						SuccessTime:    successTime2,
+						SuccessAmtSat:  200,
+						SuccessAmtMsat: 2000,
+					},
+				},
+				{
+					NodeFrom: nodeFrom,
+					NodeTo:   nodeTo,
+					History: &ecrpc.PairData{
+						FailTime:       failTime2,
+						FailAmtSat:     100,
+						FailAmtMsat:    1000,
+						SuccessTime:    successTime2,
+						SuccessAmtSat:  200,
+						SuccessAmtMsat: 2000,
+					},
+				},
+			},
+		})
+		require.NoError(t, err)
+
+		// Mock ticker with a fixed interval.
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+
+		// Start the cleanup routine.
+		server.RunCleanupRoutine(ticker)
+
+		// Wait for some time to ensure the ticker ticks at least once.
+		time.Sleep(3 * time.Second)
+
+		// After waiting for the ticker to tick, query the database to
+		// check if stale data has been removed.
+		resp, err := server.QueryAggregatedMissionControl(
+			context.Background(),
+			&ecrpc.QueryAggregatedMissionControlRequest{},
+		)
+		require.NoError(t, err)
+		require.NotNil(t, resp)
+
+		// Assert that there is one pair in the response, indicating
+		// that all stale data has been removed.
+		require.Len(t, resp.Pairs, 1)
+	})
+
+	t.Run("cleanupStaleData", func(t *testing.T) {
+		// Clear database before running the test.
+		err := clearDatabase(db)
+		require.NoError(t, err)
+
+		nodeFrom, nodeTo := generateTestKeys(t)
+
+		failTime1 := time.Now().Unix()
+		successTime1 := time.Now().Unix()
+		failTime2 := time.Now().Add(-15 * time.Minute).Unix()
+		successTime2 := time.Now().Add(-12 * time.Minute).Unix()
+
+		// Insert test data into the database.
+		_, err = server.RegisterMissionControl(context.Background(), &ecrpc.RegisterMissionControlRequest{
+			Pairs: []*ecrpc.PairHistory{
+				{
+					NodeFrom: nodeFrom,
+					NodeTo:   nodeTo,
+					History: &ecrpc.PairData{
+						FailTime:       failTime1,
+						FailAmtSat:     100,
+						FailAmtMsat:    1000,
+						SuccessTime:    successTime1,
+						SuccessAmtSat:  200,
+						SuccessAmtMsat: 2000,
+					},
+				},
+				{
+					NodeFrom: nodeFrom,
+					NodeTo:   nodeTo,
+					History: &ecrpc.PairData{
+						FailTime:       failTime2,
+						FailAmtSat:     100,
+						FailAmtMsat:    1000,
+						SuccessTime:    successTime2,
+						SuccessAmtSat:  200,
+						SuccessAmtMsat: 2000,
+					},
+				},
+				{
+					NodeFrom: nodeFrom,
+					NodeTo:   nodeTo,
+					History: &ecrpc.PairData{
+						FailTime:       failTime2,
+						FailAmtSat:     100,
+						FailAmtMsat:    1000,
+						SuccessTime:    successTime2,
+						SuccessAmtSat:  200,
+						SuccessAmtMsat: 2000,
+					},
+				},
+			},
+		})
+		require.NoError(t, err)
+
+		// Call cleanupStaleData to remove stale data.
+		server.cleanupStaleData()
+
+		// Verify that all stale data is removed.
+		resp, err := server.QueryAggregatedMissionControl(
+			context.Background(),
+			&ecrpc.QueryAggregatedMissionControlRequest{},
+		)
+		require.NoError(t, err)
+		require.NotNil(t, resp)
+		require.Len(t, resp.Pairs, 1)
+	})
+
+	t.Run("sanitizeRegisterMissionControlRequest", func(t *testing.T) {
+		// Case 1: No stale pairs.
+		t.Run("NoStalePairs", func(t *testing.T) {
+			failTime := time.Now().Add(-5 * time.Minute).Unix()
+			successTime := time.Now().Add(-2 * time.Minute).Unix()
+			req := &ecrpc.RegisterMissionControlRequest{
+				Pairs: []*ecrpc.PairHistory{
+					{
+						History: &ecrpc.PairData{
+							FailTime:    failTime,
+							SuccessTime: successTime,
+						},
+					},
+				},
+			}
+			stalePairsRemoved := server.sanitizeRegisterMissionControlRequest(req)
+			require.Equal(t, 0, stalePairsRemoved)
+			require.Len(t, req.Pairs, 1)
+		})
+
+		// Case 2: All pairs stale.
+		t.Run("AllStalePairs", func(t *testing.T) {
+			req := &ecrpc.RegisterMissionControlRequest{
+				Pairs: []*ecrpc.PairHistory{
+					{
+						History: &ecrpc.PairData{
+							FailTime:    time.Now().Add(-15 * time.Minute).Unix(),
+							SuccessTime: time.Now().Add(-12 * time.Minute).Unix(),
+						},
+					},
+					{
+						History: &ecrpc.PairData{
+							FailTime:    time.Now().Add(-25 * time.Minute).Unix(),
+							SuccessTime: time.Now().Add(-22 * time.Minute).Unix(),
+						},
+					},
+				},
+			}
+			stalePairsRemoved := server.sanitizeRegisterMissionControlRequest(req)
+			require.Equal(t, 2, stalePairsRemoved)
+			require.Empty(t, req.Pairs)
+		})
+	})
+
+	t.Run("isHistoryStale", func(t *testing.T) {
+		// Case 1: Non-stale history.
+		t.Run("NonStaleHistory", func(t *testing.T) {
+			failTime := time.Now().Add(-5 * time.Minute).Unix()
+			successTime := time.Now().Add(-2 * time.Minute).Unix()
+			history := &ecrpc.PairData{
+				FailTime:    failTime,
+				SuccessTime: successTime,
+			}
+			stale := isHistoryStale(
+				history, config.Server.HistoryThresholdDuration,
+			)
+			require.False(t, stale)
+		})
+
+		// Case 2: Stale history.
+		t.Run("StaleHistory", func(t *testing.T) {
+			failTime := time.Now().Add(-15 * time.Minute).Unix()
+			successTime := time.Now().Add(-12 * time.Minute).Unix()
+			history := &ecrpc.PairData{
+				FailTime:    failTime,
+				SuccessTime: successTime,
+			}
+			stale := isHistoryStale(
+				history, config.Server.HistoryThresholdDuration,
+			)
+			require.True(t, stale)
 		})
 	})
 }
